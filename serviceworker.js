8a4d40b7cd05075b01b6a5d88d6f41074bea8514
sopra <sopraneeno@gmail.com>
2018-02-05 23:08:22 +0900

    Revert "feat: offline google analytics implements."
    This reverts commit 8717537a7e87e03c2d1bce6bda9f36ba95041222.

--- serviceworker.js
+++ serviceworker.js
@@ -75,10 +75,6 @@ const STATIC_FILES = [
 ];
 
 //
-importScripts('./js/sw-offline-google-analytics.prod.v0.0.25.js');
-goog.offlineGoogleAnalytics.initialize();
-
-//
 //
 //
 function onInstall(event) {
@@ -104,7 +100,7 @@ function onFetch(event) {
             console.log('Found response in cache: ', response);
             return response;
           }
-          return fetch(event.request, {mode: 'no-cors'});
+          return fetch(event.request);
         }
       ).catch(function(error) {
         console.log('Error fetch in handler:', error);
