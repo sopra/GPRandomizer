8487b55804a733af15f72d1944a302363b0cdd41
sopra <sopraneeno@gmail.com>
2018-02-07 00:26:32 +0900

    refactor: refactor html attributes.

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
 //
 
 const APPLICATION_NAME = 'GPRandomizer';
-const VERSION = "1";
+const VERSION = "2";
 const CACHE_KEY = APPLICATION_NAME + VERSION;
 const APPLICATION_DIR = '/' + APPLICATION_NAME;
 const URL = location.protocol + '//' +
