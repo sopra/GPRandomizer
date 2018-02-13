2bbf537b68604d1222a619d413c49b9114e51edf
sopra <sopraneeno@gmail.com>
2018-02-13 22:11:53 +0900

    feat: minified html.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglifyes'),
     plumber = require('gulp-plumber'),
     ejs = require('gulp-ejs'),
+    htmlmin = require('gulp-htmlmin'),
     execSync = require('child_process').execSync,
     os = require('os'),
     uglify_option = {
@@ -26,15 +27,25 @@ gulp.task('serviceworker-minify', function() {
         .pipe(gulp.dest('./docs/'));
 });
 
+gulp.task('html-minify', function() {
+    return gulp.src('./src/index.html')
+        .pipe(htmlmin({
+          "html5": true,
+          "collapseWhitespace": true,
+          "minifyCSS": true,
+          "minifyJS": true
+        }))
+        .pipe(gulp.dest('./docs/'));
+});
+
 gulp.task('static-copy', function() {
     gulp.src('./src/pic/*').pipe(gulp.dest('./docs/pic/'));
-    gulp.src('./src/index.html').pipe(gulp.dest('./docs/'));
     gulp.src('./src/image/*').pipe(gulp.dest('./docs/image/'));
     gulp.src('./src/manifest.json').pipe(gulp.dest('./docs/'));
     gulp.src('./src/favicon.ico').pipe(gulp.dest('./docs/'));
     return;
 });
 
-gulp.task('default', ['randomizer-minify', 'serviceworker-minify', 'static-copy'], function() {
+gulp.task('default', ['randomizer-minify', 'serviceworker-minify', 'html-minify', 'static-copy'], function() {
     return;
 });
